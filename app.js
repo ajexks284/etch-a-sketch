50f9deaf63949c1fd4a0c7a4269a11cf186f4c11
ajexks284 <76217648+ajexks284@users.noreply.github.com>
2021-02-03 14:03:10 +0200

    Add buttons & functions for clearGrid and changeGridSize.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const container = document.querySelector('.container');
+const clearBtn = document.getElementById('clearBtn');
+const changeSizeBtn = document.getElementById('changeSizeBtn');
 
 function createGrid(gridNum) {
     let gridArea = gridNum * gridNum;
@@ -27,7 +29,36 @@ function changeColor(e) {
     e.target.style.backgroundColor = color;
 }
 
+function clearGrid() {
+    let gridItems = container.children; 
+
+    // Go through each square and set its background color to white
+    Array.from(gridItems).forEach(item => {
+        item.style.backgroundColor = '#fff';
+    })
+}
+
+function changeGridSize() {
+    let newGridSize = prompt('Enter a new size for your grid (between 1 - 100)', 16);
+
+    if (newGridSize < 1 || newGridSize > 100 || Number.isNaN(newGridSize)) {
+        alert("Enter a number between 1 and 100.");
+        changeGridSize();
+    } else {
+        clearGrid();
+        removeGrid();
+        createGrid(newGridSize);
+    }
+}
+
+function removeGrid() {
+    Array.from(container.children).forEach(item => {
+        container.removeChild(item);
+    })
+}
 
 
-
+// Event Listeners
 window.addEventListener('load', createGrid(16));
+clearBtn.addEventListener('click', clearGrid);
+changeSizeBtn.addEventListener('click', changeGridSize);
